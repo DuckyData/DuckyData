a665f0527a539d7592041f0267ee0f577638096b
msdohehrty <msdoherty@myseneca.ca>
2016-05-30 19:00:40 -0400

    up-to-date code

--- DuckyData1.0.0Alpha/Models/DesignModelClasses.cs
+++ DuckyData1.0.0Alpha/Models/DesignModelClasses.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,130 +6,201 @@ using System.Web;
 
 namespace DuckyData1._0._0Alpha.Models
 {
-    //
     public class Video : MediaFile
-{
-    public DateTime releaseDate { get; set; }
-    public string director { get; set; }
-    public string producer { get; set; }
-    public string cast { get; set; }
-    public byte[] poster { get; set; }
-}
-
-public class Audio : MediaFile
-{
-    public DateTime releaseDate { get; set; }
-    public string artist { get; set; }
-    public string album { get; set; }
-    public string genre { get; set; }
-    public int trackNumber { get; set; }
-    public string producer { get; set; }
-    public byte[] albumArt { get; set; }
-}
-
-public class MediaFile
-{
-    public int Id { get; set; }
-    //[Required]
-    //public ApplicationUser user { get; set; }
-    [Required]
-    public string fileName { get; set; }
-    [Required]
-    public string mediaType { get; set; }
-    public Audio audio { get; set; }
-    public Video video { get; set; }
-    public byte[] fileData { get; set; }
-}
-
-public class Message
-{
-    public int Id { get; set; }
-    public DateTime date { get; set; }
-    [Required]
-    [StringLength(100)]
-    public string sendTo { get; set; }
-    [Required]
-    [StringLength(100)]
-    public string Subject { get; set; }
-    [Required]
-    public string body { get; set; }
-    public ApplicationUser user { get; set; }
-    //attachment attributes
-    public byte[] Attachment { get; set; }
-    public string ContentType { get; set; }
-    public string ContentName { get; set; }
-
-}
-
-public class History
-{
-    public int Id { get; set; }
-    [Required]
-    public MediaFile file { get; set; }
-    [Required]
-    public ApplicationUser user { get; set; }
-}
-
-public class Suggestion
-{
-    public int Id { get; set; }
-    public DateTime date { get; set; }
-    [Required]
-    [StringLength(100)]
-    public string subject { get; set; }
-    [Required]
-    public string body { get; set; }
-    [Required]
-    public string category { get; set; }
-    public string notes { get; set; }
-
-    public ApplicationUser regUser { get; set; }
-}
-
-public class BugReport
-{
-    public BugReport()
     {
-        this.FollowUps = new List<FollowUp>();
+        public DateTime ReleaseDate { get; set; }
+
+        public string Director { get; set; }
+
+        public string Producer { get; set; }
+
+        public List<string> Cast { get; set; }
+
+        public byte[] Poster { get; set; }
     }
 
-    public int Id { get; set; }
-    public int repId { get; set; }
-    public DateTime date { get; set; }
-    [Required]
-    [StringLength(100)]
-    public string subject { get; set; }
-    [Required]
-    public string body { get; set; }
-    [Required]
-    public string category { get; set; }
-    [Required]
-    public string submittedBy { get; set; }
-    public ApplicationUser regUser { get; set; }
-    public ApplicationUser supportRep { get; set; }
-    public ICollection<FollowUp> FollowUps { get; set; }
-    //attachment attributes
-    public byte[] Attachment { get; set; }
-    public string ContentType { get; set; }
-    public string ContentName { get; set; }
+    public class Audio : MediaFile
+    {
+        public DateTime ReleaseDate { get; set; }
 
-}
+        public string Artist { get; set; }
 
-public class FollowUp
-{
-    public int Id { get; set; }
-    public DateTime TimeStamp { get; set; }
-    [Required]
-    [StringLength(100)]
-    public string Title { get; set; }
-    [Required]
-    public string Description { get; set; }
-    //attachment attributes
-    public byte[] Attachment { get; set; }
-    public string ContentType { get; set; }
-    public string ContentName { get; set; }
+        public string Album { get; set; }
 
-    public BugReport report { get; set; }
-}
+        public string Genre { get; set; }
+
+        public int TrackNumber { get; set; }
+
+        public string Producer { get; set; }
+
+        public byte[] AlbumArt { get; set; }
+    }
+
+    public class MediaFile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        public Audio Audio { get; set; }
+
+        public Video Video { get; set; }
+
+        public byte[] File { get; set; }
+
+        public string FileType { get; set; }
+    }
+
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime SentDate { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Recipient { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        public string Body { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        //attachment attributes
+        public byte[] Attachment { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string ContentName { get; set; }
+
+    }
+
+    public class History
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public MediaFile MediaFile { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+    }
+
+    public class Suggestion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime SentDate { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        public string Body { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+
+    public class BugReport
+    {
+        public BugReport()
+        {
+            this.FollowUps = new List<FollowUp>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser RegisteredUser { get; set; }
+
+        public ApplicationUser TechSupport { get; set; }
+
+        [Required]
+        public string SubmittedBy { get; set; }
+
+        [Required]
+        public DateTime SubmittedDate { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        public string Body { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        public ICollection<FollowUp> FollowUps { get; set; }
+
+        // Attachment Attributes
+        public byte[] Attachment { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string ContentName { get; set; }
+    }
+
+    public class FollowUp
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        //attachment attributes
+        public byte[] Attachment { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string ContentName { get; set; }
+
+        public BugReport Report { get; set; }
+    }
 
 }
