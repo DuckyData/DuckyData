cce922b92c90b31768a50fa56bbf470d6a5eb658
Faizan <faizan.chikhlikar@gmail.com>
2016-07-23 10:49:41 -0400

    Js files and images

---
+++ DuckyData1.0.0Alpha/Scripts/site.js
@@ -0,0 +1,29 @@
+﻿function BindAutocomplete(fieldID, webServiceUrl) {
+    alert($("#" + fieldID).val());
+    $( "#" + fieldID).autocomplete({
+        source: function( request, response ) {
+            $.ajax( {
+                url: webServiceUrl,
+                type: "POST",
+                dataType: "json",
+                contentType: "application/json; charset=utf-8",
+                data: '{ "searchTerm: "' + request.term + '"}',
+                success: function( data ) {
+                    var output = eval(data);
+                    response($.map(output, function (item) {
+                        var email = item.email;
+                        return {
+                            label: email,
+                            value: email
+                        }
+                    }))
+                }
+            } );
+        },
+        minLength: 3,
+        delay: 50,
+        select: function( event, ui ) {
+            $('#' + fieldID).change();
+        }
+    } );
+}
