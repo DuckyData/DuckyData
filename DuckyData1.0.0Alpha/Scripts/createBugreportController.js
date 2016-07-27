9e6b6f652974994a3e5a71be17e6957f0afd1c89
Zhaohu Zhu <Zhaohu Zhu>
2016-07-27 18:50:39 -0400

    create validation for bug report

---
+++ DuckyData1.0.0Alpha/Scripts/createBugreportController.js
@@ -0,0 +1,47 @@
+﻿duckyData.controller('createBugReportCtrl', function ($scope) {
+    $scope.createBugReportData = {
+        subject: null,
+        desc: null
+    }
+
+    $scope.createBugReportMsg = {
+        subject: null,
+        desc: null
+    }
+
+    $scope.createBugReportUI = {
+        disableBtn : false
+    }
+
+    $scope.validateSubject = function () {
+        if (!$scope.createBugReportData.subject) {
+            $scope.createBugReportMsg.subject = 'Subject is required';
+        } else {
+            if ($scope.createBugReportData.subject.length > 70) {
+                $scope.createBugReportMsg.subject = 'The field Subject must be a string with a maximum length of 70.';
+            }else{
+                $scope.createBugReportMsg.subject = null;
+            }
+        }
+        disableBtn();
+    }
+
+    $scope.validateDesc = function () {
+        if (!$scope.createBugReportData.desc) {
+            $scope.createBugReportMsg.desc = 'Description is required';
+        }
+        disableBtn();
+    }
+
+    function disableBtn() {
+        if (!$scope.createBugReportData.subject || !$scope.createBugReportData.desc) {
+            $scope.createBugReportUI.disableBtn = true;
+        } else {
+            if ($scope.createBugReportMsg.subject) {
+                $scope.createBugReportUI.disableBtn = true;
+            } else {
+                $scope.createBugReportUI.disableBtn = false;
+            }
+        }
+    }
+});
